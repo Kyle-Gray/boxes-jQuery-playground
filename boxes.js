a9ee978efcc6ffe36499adde0849935b1b9b3657
Kyle Gray <th33nd3r@gmail.com>
2016-10-19 12:55:52 -0600

    found two ways to solve exercise two

--- boxes.js
+++ boxes.js
@@ -2,6 +2,16 @@
 "use strict";
 $(document).ready(function(){
   console.log("ready");
-  $("#secretBox").css("background", "purple");
-  console.log($("#secretBox"));
+  // exercise 1
+  $("#secretBox").css("background", "white");
+  $("#secretBox").append($("<h1>Secret Box!</h1>"));
+  //exercise 2
+  // $(".boxType1").css("background", "white");
+  $(".box").each(function(){
+    if($(this).css("background-color") === "rgb(255, 0, 0)"){
+    $(this).attr("style", "background-color: purple");
+  }
+  });
+
+
 });

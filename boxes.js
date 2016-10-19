e3b00f9f80746c33b1d1b739a5afcdfa79da31e4
Kyle Gray <th33nd3r@gmail.com>
2016-10-19 14:13:53 -0600

    reformatted js file to comment our previous exercises

--- boxes.js
+++ boxes.js
@@ -3,25 +3,29 @@
 $(document).ready(function(){
   console.log("ready");
   // exercise 1
-  $("#secretBox").css("background", "white");
-  $("#secretBox").append($("<h1>Secret Box!</h1>"));
+  // $("#secretBox").css("background", "white");
+  // $("#secretBox").append($("<h1>Secret Box!</h1>"));
   //exercise 2
+  // method 1
   // $(".boxType1").css("background", "white");
-  $(".box").each(function(){
-    if($(this).css("background-color") === "rgb(255, 0, 0)"){
-    $(this).attr("style", "background-color: white");
-  }
-  });
+  // method 2
+  // $(".box").each(function(){
+  //   if($(this).css("background-color") === "rgb(255, 0, 0)"){
+  //   $(this).attr("style", "background-color: white");
+  // }
+  // });
   // exercise 3
-  $("#row1").children().addClass("boxType3");
+  //method 1
+  // $("#row1").children().addClass("boxType3");
+  // method 2
   // $("#row1").children().each(function(){
   //   $(this).addClass("boxType3");
   // });
   // exercise 4
-  $("#row4").children().last().css("display", "none");
+  // $("#row4").children().last().css("display", "none");
   // exercise 5
-  var twoDivs = $("#row2").children().slice(0,2);
-  twoDivs.removeAttr("style");
-  twoDivs.removeAttr("class");
-  console.log(twoDivs);
+  // var twoDivs = $("#row2").children().slice(0,2);
+  // twoDivs.removeAttr("style");
+  // twoDivs.removeAttr("class");
+  // console.log(twoDivs);
 });
